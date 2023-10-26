f27229b167a6ffa424da47e66077a21607e92d2e
magda <kramekm95@gmail.com>
2023-10-26 23:47:33 +0200

    Initial commit

---
+++ src/main/java/com/server/socialBees/SocialBeesApplication.java
@@ -0,0 +1,13 @@
+package com.server.socialBees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialBeesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/socialBees/SocialBeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.socialBees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
